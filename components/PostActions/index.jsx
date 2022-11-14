b6732710cd10ca2dbc6723205d56bf3ad96dc21e
Lucia-martin <90855217+Lucia-martin@users.noreply.github.com>
2022-11-13 19:09:50 -0800

    need to manually refresh

--- components/PostActions/index.jsx
+++ components/PostActions/index.jsx
@@ -6,8 +6,8 @@ import { HeartIcon as HeartIconSolid } from '@heroicons/react/24/solid'
 import {useState} from 'react'
 export default function PostActions({ onComment, onLike, onShare, totalLikes, totalComments, liked, className = "" }) {
 
-  const [isLiked, setIsLiked]=useState(liked)
-  const [totalLs, setTotalLs]=useState(totalLikes)
+  // const [isLiked, setIsLiked]=useState(liked)
+  // const [totalLs, setTotalLs]=useState(totalLikes)
 
   return (
     <div className={'flex items-center justify-between ' + className}>
@@ -19,16 +19,19 @@ export default function PostActions({ onComment, onLike, onShare, totalLikes, to
         <CommentIcon className="h-7 w-7" aria-hidden="true" />
       </button>
       <button
-        onClick={()=>{
-          onLike()
-          !isLiked ? setTotalLs(totalLs + 1) : setTotalLs(totalLs - 1)
-          setIsLiked(!isLiked)
-        }}
+        onClick={
+          onLike
+          // !isLiked ? setTotalLs(totalLs + 1) : setTotalLs(totalLs - 1)
+          // setIsLiked(!isLiked)
+        }
         className="flex flex-col items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md hover:outline-none text-gray-400 hover:text-gray-500"
       >
-        <span>{totalLs}</span>
+        {/* <span>{totalLs}</span> */}
+                <span>{totalLikes}</span>
+
         {
-          !isLiked ? <HeartIcon className="h-7 w-7" aria-hidden="true" />
+          //isliked
+          !liked ? <HeartIcon className="h-7 w-7" aria-hidden="true" />
             : <HeartIconSolid className="h-7 w-7" aria-hidden="true" />
         }
       </button>
